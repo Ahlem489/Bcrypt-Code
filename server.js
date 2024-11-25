5e039e35f58c53bd1b7eee62b8243e35c94d77ec
Ahlem489 <ftouhaahlem@gmail.com>
2024-11-25 22:00:03 +0100

    Hash and Compare Passwords Asynchronously

--- server.js
+++ server.js
@@ -8,7 +8,19 @@ const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 const bcrypt = require('bcrypt');
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    /*Store hash in your db*/
+  });
+bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    /*res == true or false*/
+  });
+bcrypt.hash('passw0rd!', 13, (err, hash) => {
+  console.log(hash);
+  //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+  bcrypt.compare('passw0rd!', hash, (err, res) => {
+    console.log(res); //true
+  });
+});
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
