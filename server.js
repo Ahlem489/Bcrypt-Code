4fb5b5a177c659ad3b2002fe90b943422a8dd096
Ahlem489 <ftouhaahlem@gmail.com>
2024-11-25 22:09:13 +0100

    Hash and Compare Passwords Asynchronously

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
 bcrypt.hash('passw0rd!', 13, (err, hash) => {
   console.log(hash);
   //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
-  bcrypt.compare('passw0rd!', hash, (err, res) => {
+bcrypt.compare('passw0rd!', hash, (err, res) => {
     console.log(res); //true
   });
 });
